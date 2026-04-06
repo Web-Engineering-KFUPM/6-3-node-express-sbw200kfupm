a715d7cdb98303f05ec9b4446be86dc384ca3e95
sbw200kfupm <s202337730@kfupm.edu.sa>
2026-04-06 11:52:13 +0300

    todo 2 create function

--- 6-3-node-express/backend/utils/random.js
+++ 6-3-node-express/backend/utils/random.js
@@ -1,1 +1,6 @@
 // TODO 2: Create & Implement getRandomInt function
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
