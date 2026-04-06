09153df6ccc8b9cf07496430b3580767195e9f68
sbw200kfupm <s202337730@kfupm.edu.sa>
2026-04-06 11:45:47 +0300

    todo 1

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,13 +1,17 @@
 // TODO 1: Import and Create express app instance
+import express from "express";
+const app = express();
 
 // TODO 1: Define server port
-
+const PORT = process.env.PORT || 3000;
 
 // TODO 4: Import and Apply CORS middleware
-
+import cors from "cors";
+app.use(cors());
 
 // TODO 5: Import and apply Morgan middleware
-
+import morgan from "morgan";
+app.use(morgan("dev"));
 
 // TODO 6.1: Create root route "/"
 
